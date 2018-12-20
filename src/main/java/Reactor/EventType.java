8806811ce089a954ded156a38e8765ef50c5452c
Lujango <935819994>
2018-12-20 14:21:34 +0800

    reactor

---
+++ src/main/java/Reactor/EventType.java
@@ -0,0 +1,13 @@
+package Reactor;
+
+/**
+ * @Author: feiweiwei
+ * @Description:
+ * @Created Date: 11:05 17/10/12.
+ * @Modify by:
+ */
+public enum EventType {
+    ACCEPT,
+    READ,
+    WRITE;
+}
